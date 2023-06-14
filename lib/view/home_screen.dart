e47b16884a46a4cf5513cdfd05d8c07442b2cb93
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-06-14 23:17:48 +0500

    3nd Commit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -10,6 +10,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
   }
 }
